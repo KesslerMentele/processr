302d0f63a0ed3f1ed2c6381e472794b120e9dda1
kess <kesslermentele@gmail.com>
2026-03-27 10:35:46 -0700

    Added missing pack-io.ts

---
+++ src/utils/pack-io.ts
@@ -0,0 +1,31 @@
+import type { GamePack } from "../models";
+
+export function importPackFromFile(): Promise<GamePack> {
+  return new Promise((resolve, reject) => {
+    const input = document.createElement("input");
+    // eslint-disable-next-line functional/immutable-data
+    input.type = "file";
+    // eslint-disable-next-line functional/immutable-data
+    input.accept = ".json";
+    // eslint-disable-next-line functional/immutable-data
+    input.onchange = () => {
+      const file = input.files?.[0];
+      if (file === undefined) { reject(new Error("No file selected")); return; }
+      void file.text().then(text => { resolve(JSON.parse(text) as GamePack); });
+    };
+    input.click();
+  });
+}
+
+export function exportPackToFile(pack: GamePack): void {
+  const url = URL.createObjectURL(
+    new Blob([JSON.stringify(pack, null, 2)], { type: "application/json" })
+  );
+  const a = document.createElement("a");
+  // eslint-disable-next-line functional/immutable-data
+  a.href = url;
+  // eslint-disable-next-line functional/immutable-data
+  a.download = `${pack.name}-${pack.version}.json`;
+  a.click();
+  URL.revokeObjectURL(url);
+}
